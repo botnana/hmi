49f9dd6250044e8511c514b36658b0cbab5c4301
ccwu <ccwu660601@mapacode.tw>
2015-03-12 19:17:29 +0800

    CCWU: 修改 blog 內的 posts 內容。

--- posts/zh-Hant/botnana-platform-architecture.md
+++ posts/zh-Hant/botnana-platform-architecture.md
@@ -1,13 +1,10 @@
-# Botnana 工業物聯網開發平台
-
-Botnana 為一款工業物聯網開發平台。此一平台整合了以下服務：
+BotBone 使用動程科技的 Botnana 工業物聯網開發平台。此一平台整合了以下服務：
 
 * 小型的部落格。以提供產品的文件。
 * 硬體控制語言 MSL 以及其執行環境。此一執行環境可透過雲端 API 存取。
 * 簡易的 SCADA/HMI。
-* 其他，待開發。
 
-此一平台的各種服務透過 git://botnana.mapacode.tw 提供軟體更新下載。
+Botnana 平台的各種服務透過 git://botnana.mapacode.tw 提供軟體更新下載。
 
 ## 執行環境
 
--- posts/zh-Hant/posts.js
+++ posts/zh-Hant/posts.js
@@ -15,6 +15,12 @@ var _posts = [
         md: 'botbone-tutorials.md',
         published: '2015-02-25',
         preview: 'BotBone Tutorials'
+    },
+    {
+        title: 'Botnana 工業物聯網開發平台',
+        md: 'botnana-platform-architecture.md',
+        published: '2015-02-25',
+        preview: 'BotBone 使用的工業物聯網開發環境整合了微型部落格、簡易 SCADA/HMI 以及硬體控制語言 MSL 及其執行環境。'
     }
 ];
 
