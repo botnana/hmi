ef2180ef92ae57097c392d745e8b1438e060d84b
ccwu <ccwu660601@mapacode.tw>
2015-04-02 14:59:41 +0800

    CCWU: added author.

--- posts/zh-Hant/posts.js
+++ posts/zh-Hant/posts.js
@@ -8,18 +8,21 @@ var _posts = [
         title: 'Botbone 入門',
         md: 'botbone-getting-started.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'BotBone Getting Started Guide'
     },
     {
         title: 'BotBone 教學',
         md: 'botbone-tutorials.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'BotBone Tutorials'
     },
     {
         title: 'Botnana 工業物聯網開發平台',
         md: 'botnana-platform-architecture.md',
         published: '2015-02-25',
+        author: 'Sirius Wu',
         preview: 'BotBone 使用的工業物聯網開發環境整合了微型部落格、簡易 SCADA/HMI 以及硬體控制語言 MSL 及其執行環境。'
     }
 ];
