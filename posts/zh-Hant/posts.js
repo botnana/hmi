fb63e1d13e58913bb17fb054a4bfa5e9c41f82fa
ccwu <ccwu660601@mapacode.tw>
2015-04-20 12:42:20 +0800

    CCWU: replaced sections with tags.

--- posts/zh-Hant/posts.js
+++ posts/zh-Hant/posts.js
@@ -3,33 +3,31 @@
  */
 'use strict';
 
-var sections = [{
-    section: 'tutorials',
-    posts: [
-        {
-            title: 'Botbone 入門',
-            md: 'botbone-getting-started.md',
-            img: 'botbone.png',
-            published: '2015-02-25',
-            author: 'Sirius Wu',
-            preview: 'BotBone 帶你進自動化及工業 4.0 的世界，你可以透過它學到...'
-        },
-        {
-            title: 'BotBone 教學',
-            md: 'botbone-tutorials.md',
-            published: '2015-02-25',
-            author: 'Sirius Wu',
-            preview: '使用 BotBone 的一些小技巧...'
-        },
-        {
-            title: 'Botnana 工業物聯網開發平台',
-            md: 'botnana-platform-architecture.md',
-            published: '2015-02-25',
-            author: 'Sirius Wu',
-            preview: 'BotBone 內建的工業物聯網開發環境整合了微型部落格、簡易 SCADA/HMI 以及硬體控制語言 MSL 及其執行環境。'
-        }
-    ]
-}];
+var posts = {
+    data: [{
+        "title": "Botbone 入門",
+        "md": "botbone-getting-started.md",
+        "img": "botbone.png",
+        "published": "2015-02-25",
+        "author": "Sirius Wu",
+        "preview": "BotBone 帶你進自動化及工業 4.0 的世界，你可以透過它學到...",
+        "tags": "tutorials"
+    }, {
+        "title": "BotBone 教學",
+        "md": "botbone-tutorials.md",
+        "published": "2015-02-25",
+        "author": "Sirius Wu",
+        "preview": "使用 BotBone 的一些小技巧...",
+        "tags": "tutorials"
+    }, {
+        "title": "Botnana 工業物聯網開發平台",
+        "md": "botnana-platform-architecture.md",
+        "published": "2015-02-25",
+        "author": "Sirius Wu",
+        "preview": "BotBone 內建的工業物聯網開發環境整合了微型部落格、簡易 SCADA/HMI 以及硬體控制語言 MSL 及其執行環境。",
+        "tags": "tutorials"
+    }]
+};
 
-module.exports = sections;
+module.exports = posts;
 
